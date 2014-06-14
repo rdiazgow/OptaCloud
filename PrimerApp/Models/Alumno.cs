084c995bf0ed47ceb3efb45aeba242698ad639fb
rdiazgow <djjosefs@yahoo.com>
2014-06-14 15:44:19 -0500

    Initial commit

---
+++ PrimerApp/Models/Alumno.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PrimerApp.Models
+{
+    public class Alumno
+    {
+        public int AlumnoId { get; set; }
+        public string Nombre { get; set; }
+    }
+}
